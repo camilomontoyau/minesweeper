749b247e013edfd308beb5577c20f6a2098be83a
camilo <camilomontoya2@gmail.com>
2018-11-15 20:19:14 -0500

    games: list, get, create

--- server/server.js
+++ server/server.js
@@ -3,18 +3,35 @@ require('./config');
 const express = require('express');
 const server = express();
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const Game = require('./models/game');
 
-const bodyParser = require('body-parser');
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 
 server.get('/', (req, res) => {
-  const game = new Game({ owner: 'Camilo' });
+  return res.status(200).json('welcome to minesweeper!');
+});
+
+server.get('/games', (req, res) => {
+  Game.find(req.query, (err, games) => {
+    if (err) return errorHandler(res, err);
+    return res.status(200).json(games);
+  });
+});
+
+server.get('/games/:id', (req, res) => {
+  Game.findById(req.params.id, (err, game) => {
+    if (err) return errorHandler(res, err);
+    return res.status(200).json(game);
+  });
+});
+
+server.post('/games', (req, res) => {
+  const game = new Game(req.body);
   game.save((err, createdGame) => {
-    if (err) throw err;
-    return res.json(createdGame);
+    if (err) return errorHandler(res, err);
+    return res.json(cleanCells(createdGame));
   });
 });
 
@@ -29,3 +46,29 @@ mongoose.connect(
 server.listen(process.env.PORT, () => {
   console.log('Server Online ==> port: ', process.env.PORT);
 });
+
+function errorHandler(res, err) {
+  console.log('error', err);
+  let status = null;
+  switch (err.name) {
+    case 'ValidationError':
+      status = 400;
+      break;
+    default:
+      status = 500;
+  }
+  return res
+    .status(status)
+    .json({ error: err.message || 'internal server error' });
+}
+
+function cleanCells(game) {
+  const responseObject = game.toObject();
+  responseObject.board = responseObject.board.map(row => {
+    return row.map(cell => {
+      const { state } = cell;
+      return { state };
+    });
+  });
+  return responseObject;
+}
