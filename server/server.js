d3649320273cd3c43c198ff2b75651cc239e7963
camilo <camilomontoya2@gmail.com>
2018-11-18 16:00:26 -0500

    minesweeper icons

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const Game = require('./models/game');
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 
-server.get('/', express.static(`${__dirname}/static`));
+server.get('/*', express.static(`${__dirname}/static`));
 
 server.get('/games', (req, res) => {
   Game.find(req.query, (err, games) => {
